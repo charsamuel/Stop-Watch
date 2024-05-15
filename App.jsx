3013cdc4031c73a032e4373ff7348e4883a78cf8
Charity Samuel <135695024+charsamuel@users.noreply.github.com>
2024-05-15 12:00:18 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,16 +1,8 @@
-// import DigitalClock from "./DigitalClock"
-// import ComponentA from "./ComponentA";
-
-import MyComponent from "./MyComponent";
 import StopWatch from "./StopWatch.jsx";
 
 function App() {
   return (
     <StopWatch/>
-    // <MyComponent />
-    // <ComponentA />
-
-    // <DigitalClock/>
   );
 }
 
